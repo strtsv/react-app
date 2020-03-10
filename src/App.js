acc2c6e38c8828314c796560a9c529e9d6fa8960
jsusilo <enr@bk.ru>
2020-03-10 22:35:31 +0500

    set up the form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import Header from "./Header";
+import RegistrationForm from './RegistrationForm';
+
+const menu = [
+  {
+    link: "/articles",
+    label: "Articles"
+  },
+  {
+    link: "/contacts",
+    label: "Contacts"
+  },
+  {
+    link: "/posts",
+    label: "Posts"
+  }
+];
 
 class App extends Component {
   render() {
-    return <div>Hello world</div>;
+    return (
+      <div>
+        <Header items={menu} />
+        <RegistrationForm />
+      </div>
+    );
   }
 }
 
