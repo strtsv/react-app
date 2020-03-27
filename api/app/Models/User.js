e6ec11a780310fc610fdfc3b831eb287376841bf
jsusilo <enr@bk.ru>
2020-03-27 18:16:16 +0500

    set up the api

--- api/app/Models/User.js
+++ api/app/Models/User.js
@@ -1,24 +1,22 @@
-"use strict";
+'use strict'
 
-const Model = use("Model");
-const Hash = use("Hash");
+const Model = use('Model')
+const Hash = use('Hash')
 
 class User extends Model {
-  static boot() {
-    super.boot();
+  static boot () {
+    super.boot()
 
-    this.addHook("beforeSave", async userInstance => {
+    this.addHook('beforeSave', async (userInstance) => {
       if (userInstance.dirty.password) {
-        userInstance.password = await Hash.make(userInstance.password);
+        userInstance.password = await Hash.make(userInstance.password)
       }
-    });
+    })
   }
-  tokens() {
-    return this.hasMany("App/Models/Token");
-  }
-  projects() {
-    return this.hasMany("App/Models/Project");
+
+  tokens () {
+    return this.hasMany('App/Models/Token')
   }
 }
 
-module.exports = User;
+module.exports = User
